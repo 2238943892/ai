1bb1c96863c500ca47cbe91cc7a3bdb30acdfbf1
2238943892 <cixufuzuy65@gmail.com>
2026-03-28 20:50:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
+import gradio as gr
 import os
+
+def test():
+    return "OK"
+
+with gr.Blocks() as demo:
+    gr.Markdown("# 测试页面")
+
+    btn = gr.Button("点我")
+    out = gr.Textbox()
+
+    btn.click(fn=test, outputs=out)
 
 demo.launch(
     server_name="0.0.0.0",
